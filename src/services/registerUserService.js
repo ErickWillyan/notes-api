7bce6cebdce920f1d062053420602c6ad9f5704a
ErickWillyan <141357058+ErickWillyan@users.noreply.github.com>
2024-02-19 17:50:32 -0300

    RegisterUserService update

--- src/services/registerUserService.js
+++ src/services/registerUserService.js
@@ -11,14 +11,12 @@ const RegisterUserService = async ({ name, email, password }) => {
       throw new Error("Este usuário já existe");
     }
 
-    const nameHash = await bcrypt.hash(name, 8);
-    const emailHash = await bcrypt.hash(email, 8);
     const passwordHash = await bcrypt.hash(password, 8);
 
     const user = await prisma.Users.create({
       data: {
-        email: emailHash,
-        name: nameHash,
+        email,
+        name,
         password: passwordHash,
       },
     });
